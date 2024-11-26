3e06bfa6bd77f294d6eaad05df096fe74d313253
Schurlo <georg.engelsmann@gmail.com>
2024-11-26 18:16:21 +0100

    Wiederherstellen und alle Löschen

--- Strafenkatalog/ViewModel/ArchivePlayerViewModel.cs
+++ Strafenkatalog/ViewModel/ArchivePlayerViewModel.cs
@@ -40,11 +40,37 @@ namespace Strafenkatalog.ViewModel
         [RelayCommand]
         public async Task DeleteFine(FineGiven fine)
         {
-            if (await Shell.Current.DisplayAlert("Löschen", "Wollen Sie wirklich die Strafe löschen?", "Ja", "Nein"))
+            if (await Shell.Current.DisplayAlert("Wiederherstellen | Löschen", "Wollen Sie die Strafe wiederherstellen oder löschen?", "Löschen", "Wiederherstellen"))
             {
                 _dbContext.FinesGiven.Remove(fine);
                 _dbContext.SaveChanges();
                 FineList.Remove(fine);
+            }
+            else
+            {
+                FineType? finetype = await _dbContext.FineTypes.FindAsync(fine.FineTypeId);
+                fine.Paid = false;
+                Player.Betrag += finetype.Sum * fine.Count;
+                _dbContext.Players.Update(Player);
+                _dbContext.FinesGiven.Update(fine);
+                _dbContext.SaveChanges();
+                FineList.Remove(fine);
+            }
+        }
+
+        [RelayCommand]
+        public async Task DeleteAllFine()
+        {
+            if(await Shell.Current.DisplayAlert("Löschen", "Wollen Sie wirklich alle Strafen löschen?", "Ja", "Nein"))
+            {
+                var cFineList = FineList.ToList();
+
+                foreach(FineGiven fine in cFineList)
+                {
+                    _dbContext.FinesGiven.Remove(fine);
+                    _dbContext.SaveChanges();
+                    FineList.Remove(fine);
+                }
             }
         }
     }
